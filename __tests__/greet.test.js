687f4b825d4e98240fc2f56c02248a3147a74ebb
Billy Bunn <williammbunn@gmail.com>
2019-03-18 15:55:21 -0700

    Adds first test for non-string inputs

--- __tests__/greet.test.js
+++ __tests__/greet.test.js
@@ -1,3 +1,24 @@
 'use strict';
 
-const greeting = require('../lib/greet');
+const greet = require('../lib/greet');
+describe('returns null when you supply non-string values', () => {
+  it('number', () => {
+    expect(greet(0)).toBeNull(); // number
+  });
+  it('boolean', () => {
+    expect(greet(true)).toBeNull(); // boolean
+  });
+  it('null', () => {
+    expect(greet(null)).toBeNull(); // null
+  });
+  it('undefined', () => {
+    expect(greet(undefined)).toBeNull(); // undefined
+  });
+  it('object', () => {
+    expect(greet({})).toBeNull(); // object
+  });
+  it('symbol', () => {
+    expect(greet(Symbol('foo'))).toBeNull(); // symbol
+  });
+
+});
