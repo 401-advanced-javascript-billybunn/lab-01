fa69ad4ea7f0645a5a0f55c6560562f5de0448b5
Billy Bunn <williammbunn@gmail.com>
2019-03-18 16:17:51 -0700

    Adds arithmetic test for improper function use

--- __tests__/arithmetic.test.js
+++ __tests__/arithmetic.test.js
@@ -2,3 +2,25 @@
 
 const arithmetic = require('../lib/arithmetic.js');
 
+describe('improper use', () => {
+  it('returns null if invoked with no arguments', () => {
+    expect(arithmetic.add()).toBeNull();
+    expect(arithmetic.subtract()).toBeNull();
+  });
+
+  it('returns null if invoked with only one argument', () => {
+    expect(arithmetic.add(8)).toBeNull();
+    expect(arithmetic.subtract(8)).toBeNull();
+  });
+
+  it('ignores any arguments passed in after the first two', () => {
+    expect(arithmetic.add(1, 2, 3)).toBe(3);
+    expect(arithmetic.subtract(1, 2, 3)).toBe(-1);
+  });
+
+  it('returns null if invoked with an argument that isn\'t a number', () => {
+    expect(arithmetic.add('1', 2)).toBeNull();
+    expect(arithmetic.subtract(2, '1')).toBeNull();
+  });
+
+});
