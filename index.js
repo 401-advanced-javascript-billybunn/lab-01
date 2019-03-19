1c0eb6e8f09d1d8eac00bb52b0e784252bc969f8
Billy Bunn <williammbunn@gmail.com>
2019-03-18 22:23:45 -0700

    Updates console.logs

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 'use strict';
-
+const faker = require('faker');
 const greet = require('./lib/greet.js');
 const math = require('./lib/arithmetic.js');
-console.log(greet('JOHN'));
+console.log(greet('JOHN')); // JOHN
+console.log(math.add('not and object')); // null cause not a number
+console.log(math.add()); // null cause empty
 console.log(math.add(1, 3)); // 4
+console.log(math.add(1, 3, 5)); // 9
+console.log(math.add(1, 3, '5')); // null
+console.log(math.add([])); // null
+
 console.log(math.subtract(1, 3)); // -2
 console.log(math.multiply(2, 3)); // 6
 console.log(math.divide(6, 2)); // 3
-
